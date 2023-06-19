f04393c35d5dc272ea56f06dc59e413948cd45ce
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-06-19 09:47:09 +0200

    Adding set and patch value for form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,35 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
 
-  @ViewChild('f') singedUpForm: NgForm;
+  @ViewChild('f') singUpForm: NgForm;
 
-  defaultQuestion = 'pet';
-
+  defaultQuestion = 'teacher';
   answer = '';
+  genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.singUpForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // })
+
+    this.singUpForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    })
+    console.log('Sugestion clicked')
   }
 
 
   onSubmit(){
-    console.log(this.singedUpForm)
+    console.log(this.singUpForm)
   }
 
   // onSubmit(form: NgForm){
